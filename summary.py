094c2f5cb49e93c6d7183c11e31dbaa73ed53675
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-09-17 17:18:24 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -11,5 +11,17 @@ if __name__ == "__main__":
     model = yolo_body(input_shape, anchors_mask, num_classes)
     model.summary()
 
+    # mobilenetv1 41,005,757
+    # mobilenetv2 39,124,541
+    # mobilenetv3 40,043,389
+
+    # 修改了PANET的mobilenetv1  12,754,109
+    # 修改了PANET的mobilenetv2  10,872,893
+    # 修改了PANET的mobilenetv3  11,791,741
+
+    # 修改了PANET的mobilenetv1-0.25     680,381
+    # 修改了PANET的mobilenetv2-0.5      2,370,541
+    # 修改了PANET的mobilenetv3-0.75     6,315,309
+    
     # for i,layer in enumerate(model.layers):
     #     print(i,layer.name)
