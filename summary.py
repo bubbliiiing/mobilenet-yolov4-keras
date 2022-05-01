77c4aab983ba44b7162d15f76bfb581bc60f811d
Bubbliiiing <3323290568@qq.com>
2022-05-01 12:10:57 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,14 +2,29 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 from nets.yolo import yolo_body
+from utils.utils import net_flops
 
 if __name__ == "__main__":
-    input_shape     = [416, 416, 3]
+    input_shape     = [416, 416]
     anchors_mask    = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
     num_classes     = 80
-
-    model = yolo_body(input_shape, anchors_mask, num_classes, backbone="densenet121")
+    backbone        = 'mobilenetv1'
+    
+    model = yolo_body([input_shape[0], input_shape[1], 3], anchors_mask, num_classes, backbone=backbone)
+    #--------------------------------------------#
+    #   查看网络结构网络结构
+    #--------------------------------------------#
     model.summary()
+    #--------------------------------------------#
+    #   计算网络的FLOPS
+    #--------------------------------------------#
+    net_flops(model, table=False)
+    
+    #--------------------------------------------#
+    #   获得网络每个层的名称与序号
+    #--------------------------------------------#
+    # for i,layer in enumerate(model.layers):
+    #     print(i,layer.name)
 
     # mobilenetv1 41,005,757
     # mobilenetv2 39,124,541
@@ -23,5 +38,3 @@ if __name__ == "__main__":
     # 修改了PANET的mobilenetv2-0.5      2,370,541
     # 修改了PANET的mobilenetv3-0.75     6,315,309
     
-    for i,layer in enumerate(model.layers):
-        print(i,layer.name)
